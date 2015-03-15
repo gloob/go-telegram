87cb711dd71ac8201914090ccd4bda4b677c65d0
Alejandro Leiva <alejandro.leiva@cocomore.com>
2015-03-15 10:41:38 +0100

    Exposing all tgl lib references into Go.

--- tgl/tgl.go
+++ tgl/tgl.go
@@ -1,1 +1,8 @@
 package tgl
+
+// #cgo CFLAGS: -I ./lib
+// #cgo LDFLAGS: -L ./lib/tgl/libs -l tgl
+// #include "tgl.go.h"
+import "C"
+
+func login(state C.struct_tgl_state) C.struct_tgl_state { return state }
---
+++ tgl/tgl.go.h
@@ -0,0 +1,6 @@
+#ifndef GO_TGL_H
+#define GO_TGL_H
+
+#include <tgl/tgl.h>
+
+#endif // GO_TGL_H
